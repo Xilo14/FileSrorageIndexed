547765a45a22a66f5f6480961287ae8107a5451e
Xilosof <pro100temi444@gmail.com>
2021-08-12 18:56:25 +0300

    feat: create a project for testing Lemmalizer

---
+++ LemmaTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using FSIApi;
+
+namespace LemmaTest {
+    class Program {
+        static void Main(string[] args) {
+            var api = new FSIApi.FSIApi("https://localhost:5001");
+
+            Console.WriteLine(api.GetFilesCount().Result);
+
+            var path = "test.png";
+
+            //api.UploadFile("test.png", "Тестовый файл", path).Wait();
+
+
+            var result = api.SearchFiles("Тестовый файл").Result.ToList();
+
+            var outPath = "out.png";
+
+            api.GetFile(result[0].Id, outPath).Wait();
+
+            Console.WriteLine("Complete!");
+            Console.ReadLine();
+
+        }
+    }
+}
