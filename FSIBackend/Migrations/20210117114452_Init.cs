b4a2dce4adfdc8a762fe2986d39ec3817c3fe682
Xilosof <pro100temi444@gmail.com>
2021-08-12 18:56:37 +0300

    feat: Create a migrations

---
+++ FSIBackend/Migrations/20210117114452_Init.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace FSIBackend.Migrations
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Files",
+                columns: table => new
+                {
+                    FileId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    OriginalName = table.Column<string>(type: "text", nullable: true),
+                    Path = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    Content = table.Column<string>(type: "text", nullable: true),
+                    FileExtension = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Files", x => x.FileId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Lemmas",
+                columns: table => new
+                {
+                    LemmaId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Value = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lemmas", x => x.LemmaId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LemmaEntries",
+                columns: table => new
+                {
+                    LemmaEntryId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Count = table.Column<int>(type: "integer", nullable: false),
+                    FieldEntry = table.Column<int>(type: "integer", nullable: false),
+                    FileId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LemmaEntries", x => x.LemmaEntryId);
+                    table.ForeignKey(
+                        name: "FK_LemmaEntries_Files_FileId",
+                        column: x => x.FileId,
+                        principalTable: "Files",
+                        principalColumn: "FileId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LemmaEntries_FileId",
+                table: "LemmaEntries",
+                column: "FileId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "LemmaEntries");
+
+            migrationBuilder.DropTable(
+                name: "Lemmas");
+
+            migrationBuilder.DropTable(
+                name: "Files");
+        }
+    }
+}
---
+++ FSIBackend/Migrations/20210117125236_fixEntry.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FSIBackend.Migrations
+{
+    public partial class fixEntry : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LemmaId",
+                table: "LemmaEntries",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LemmaEntries_LemmaId",
+                table: "LemmaEntries",
+                column: "LemmaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LemmaEntries_Lemmas_LemmaId",
+                table: "LemmaEntries",
+                column: "LemmaId",
+                principalTable: "Lemmas",
+                principalColumn: "LemmaId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LemmaEntries_Lemmas_LemmaId",
+                table: "LemmaEntries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_LemmaEntries_LemmaId",
+                table: "LemmaEntries");
+
+            migrationBuilder.DropColumn(
+                name: "LemmaId",
+                table: "LemmaEntries");
+        }
+    }
+}
